4e93b2c63fd939f817caa1e2254d2d23efdae5df
Janus Weil <janus.weil@dfs.de>
2024-09-24 16:37:27 +0300

    ShellTabBarSample: reproducer for a problem with Sharpnado.Tabs.Maui on Android

--- 8.0/Navigation/ShellTabBarSample/ShellTabBarSample/MauiProgram.cs
+++ 8.0/Navigation/ShellTabBarSample/ShellTabBarSample/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace ShellTabBarSample;
+﻿using Sharpnado.Tabs;
+
+namespace ShellTabBarSample;
 
 public static class MauiProgram
 {
@@ -7,12 +9,13 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseSharpnadoTabs(loggerEnable: false)
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                fonts.AddFont("fa_solid.ttf", "FontAwesome");
-            });
+				fonts.AddFont("fa_solid.ttf", "FontAwesome");
+			});
 
 		return builder.Build();
 	}
